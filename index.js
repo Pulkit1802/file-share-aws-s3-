7895be11dcc6bb517c7606dae1f162b223cda252
Your Name <pulkitmlk11@gmail.com>
2022-10-24 08:22:27 +0530

    cloud db connection

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config({path: "./config.env"});
 
 const app = require("./app")
 
-mongoose.connect(process.env.DATABASE_LOCAL, {}).then(() => console.log("DataBase Connected"))
+mongoose.connect(process.env.DATABASE, {}).then(() => console.log("DataBase Connected"))
 
 const port = process.env.PORT;
 
